d1ab5a58439a312fe7196382d609e14aa2948c3c
roger <rwrobson@gmail.com>
2023-06-05 19:33:24 +0200

    Removed Elf object (superfluous) and cleaned up code.

--- 01.py
+++ 01.py
@@ -1,18 +1,4 @@
-class ElfList:
-    def __init__(self, first_item):
-        self.items = [ first_item ]
-        self.total_calories = first_item
-
-    def add_item(self, item):
-        self.items.append(item)
-        self.total_calories += item
-        
-    def __repr__(self):
-        return self.total_calories
-
-elves = []
-elf_with_most_calories = None
-current_elf = None
+## Calorie counting https://adventofcode.com/2022/day/1
 
 calorie_list = """
 1000
@@ -30,23 +16,22 @@ calorie_list = """
 
 10000"""
 
+new_total = True
+elf_calorie_totals = []
 
 for calorie_string in calorie_list.split("\n"):
     if calorie_string == "":
-        current_elf = None
+        new_total = True
     else:
         calorie_value = int(calorie_string)
-        if not current_elf:
-            current_elf = ElfList(calorie_value)
-            elves.append(current_elf)
+        if new_total:
+            elf_calorie_totals.append(calorie_value)
+            new_total = False
         else:
-            current_elf.add_item(calorie_value)
-    if current_elf and (not elf_with_most_calories or current_elf.total_calories > elf_with_most_calories.total_calories):
-        elf_with_most_calories = current_elf
+            elf_calorie_totals[-1] = elf_calorie_totals[-1] + calorie_value
 
-elf_cals = [x.total_calories for x in elves]
-elf_cals_sorted = list(reversed(sorted(elf_cals)))
+elf_calorie_totals_sorted = list(reversed(sorted(elf_calorie_totals)))
 
-print (elf_cals)
-print (elf_cals_sorted)
-print (elf_cals_sorted[0] + elf_cals_sorted[1] + elf_cals_sorted[2] )
+print ("Answer to part 1: %d" % elf_calorie_totals_sorted[0])
+
+print ("Answer to part 2: %d" % (elf_calorie_totals_sorted[0] + elf_calorie_totals_sorted[1] + elf_calorie_totals_sorted[2]))
